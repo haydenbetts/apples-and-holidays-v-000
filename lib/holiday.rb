cdbbe58bc0a80da8d71718233e7205f54da76c79
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 20:52:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,6 +38,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  binding.pry
   holiday_hash[season][holiday_name] << supply_array
   return holiday_hash
 end
