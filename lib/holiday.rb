acd9c88568d66c36eb8c0ec804840f2a284a8927
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 21:51:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,6 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, season_data|
     season_upcase = season[0].upcase + season[1..-1]
     puts "#{season_upcase}:"
+    binding.pry
     holiday_hash.values.each do |season_data|
       season_data.each do |holiday, supplies|
         puts "  #{clean_holiday_symbol(holiday)}: #{supplies.join(', ')}"
