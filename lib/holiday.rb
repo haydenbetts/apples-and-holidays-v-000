500c02a7f546f4baa14bc3d3098c5a492e32f1c8
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 20:58:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash.each do |current_season, holidays_hash|
     if current_season == season
-      holidays_hash do |holiday, supplies|
+      holidays_hash.each do |holiday, supplies|
         supplies << supply_array
       end
     end
