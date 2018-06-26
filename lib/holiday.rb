5acfd5aed888b8fec80e9badfd13f97c459a31e2
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 21:41:28 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,11 @@ def all_supplies_in_holidays(holiday_hash)
   end
 end
 
+def clean_holiday_symbol(season)
+  holiday.to_s.sub("_", " ")
+  
+end
+
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
